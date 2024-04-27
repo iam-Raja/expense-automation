7869d9b8c3dc8c452c91e4edcaca485fa61615f1
iam-raja <rajapeta45@gmail.com>
2024-04-27 11:01:41 +0530

    demo-commit

---
+++ my.sql.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+USERID=$(id -u)
+Timestamp=$(date +%F-%H-%M-%S)
+script_name=$(echo $0|cut -d "." -f1)
+log_file=/tmp/$script_name-$Timestamp.log
+R="\e[31M"
+G="\e[32M"
+Y="\e[33M"
+N="\e[0M"
+
+if [$USERID -ne 0]
+then echo "switch to super user"
+else " u r super user"
+fi
+validate(){
+    If [ $1 -ne 0 ]
+    then echo -e "$2 ..... $R Failed $N"
+    exit 1
+    else echo -e "$2 ..... $G success $N"
+    fi
+}
+dnf install mysql-server -y &>>$log_file
+validate $? "installing mysql"
+
+systemctl enable mysqld &>>$log_file
+validate $? "enabling mysqld"
+
+systemctl start mysqld &>>$log_file
+validate $? "starting mysqld"
+
+#mysql -h db.
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$log_file
+validate $? "setting up root password"
